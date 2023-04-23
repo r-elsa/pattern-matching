b4c73cc45359a67c68cef34a30ffea217f3dd9f4
Elsa <elsaryynanen@outlook.com>
2023-04-23 16:29:18 +0300

    autocomplete almost working

--- main.cpp
+++ main.cpp
@@ -5,177 +5,176 @@
 #include <iostream>
 #include <fstream>
 #include <json/json.h>
-#include <algorithm> 
+#include <algorithm>
 #include <functional>
 #include <tuple>
 #include "suffixtrie_hashmap.cpp"
 
 using namespace std;
 
-class APICall{ // Main class for doing API call to New York times and dfor parsing data and creating vector of strings 
-   public:
-         APICall(){ //Constructor
-         cout << "Instance created of APICall." << endl;
-      }
+class APICall
+{ // Main class for doing API call to New York times and dfor parsing data and creating vector of strings
+public:
+    APICall()
+    { // Constructor
+        cout << "Instance created of APICall." << endl;
+    }
 
-    int apicall(string &apiadress, string &authkey){
-            CURL *curl;
-            CURLcode res;
-            std::string stringOfWords;
+    int apicall(string &apiadress, string &authkey)
+    {
+        CURL *curl;
+        CURLcode res;
+        std::string stringOfWords;
 
-            curl_global_init(CURL_GLOBAL_DEFAULT);
-            curl = curl_easy_init();
-            if (curl) {
-                string url = apiadress + authkey;    // concatination of url and auth key 
-                curl_easy_setopt(curl, CURLOPT_URL, url.c_str());    // set url
-                curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, GetSizeOfDatafromAPI);
-                curl_easy_setopt(curl, CURLOPT_WRITEDATA, &stringOfWords);
-                res = curl_easy_perform(curl);   // request prints to stdout
+        curl_global_init(CURL_GLOBAL_DEFAULT);
+        curl = curl_easy_init();
+        if (curl)
+        {
+            string url = apiadress + authkey;   // concatination of url and auth key
+            curl_easy_setopt(curl, CURLOPT_URL, url.c_str()); // set url
+            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, GetSizeOfDatafromAPI);
+            curl_easy_setopt(curl, CURLOPT_WRITEDATA, &stringOfWords);
+            res = curl_easy_perform(curl); // request prints to stdout
 
-                if (res != CURLE_OK) {  // error checking
-                std::cerr << "Error during curl request: " 
-                            << curl_easy_strerror(res) << std::endl;
+            if (res != CURLE_OK)
+            { // error checking
+                std::cerr << "Error during curl request: "
+                          << curl_easy_strerror(res) << std::endl;
+            }
+            curl_easy_cleanup(curl);
+        }
+        else
+        {
+            std::cerr << "Error initializing curl." << std::endl;
+        }
+
+        curl_global_cleanup();
+        std::ofstream file("words.json");
+        file << stringOfWords;
+        return 0;
+    }
+
+    static size_t GetSizeOfDatafromAPI(void *data, size_t size, size_t nmemb, void *words)
+    { // returns size of data in order to create vector
+        ((std::string *)words)->append((char *)data, size * nmemb);
+        return size * nmemb;
+    }
+
+    string dataparsing(void)
+    { // parses json data from words.json file, creates strings and splits strings
+        string singleString;
+        std::ifstream file_input("words.json");
+        Json::Reader reader;
+        Json::Value obj;
+        reader.parse(file_input, obj);
+        const Json::Value &jsonofarticles = obj["response"]["docs"]; //
+        for (int i = 0; i < jsonofarticles.size(); i++)
+        {
+            string abstract = jsonofarticles[i]["abstract"].asString();
+            string lead_paragraph = jsonofarticles[i]["lead_paragraph"].asString();
+            string abstract_leadparagraph = abstract; // lead_paragraph;
+
+            std::string word;
+            for (auto letter : abstract_leadparagraph)
+            {
+
+                if (letter == ' ' or letter == '.' or letter == ',')
+                {
+                    if ((!word.empty()))
+                    {
+                        singleString += word + ' ';
+                        word.clear();
+                    }
                 }
-                curl_easy_cleanup(curl);
-                } else {
-                    std::cerr << "Error initializing curl." << std::endl;}
-
-            curl_global_cleanup();
-            std::ofstream file("words.json");
-            file << stringOfWords;
-            return 0;    
-            }
- 
-      static size_t GetSizeOfDatafromAPI(void *data, size_t size, size_t nmemb, void *words) {  // returns size of data in order to create vector
-            ((std::string*)words)-> append((char*) data, size * nmemb);
-            return size * nmemb;
-            }
-      
-      string dataparsing(void){  // parses json data from words.json file, creates strings and splits strings
-            string singleString;
-            std::ifstream file_input("words.json");
-            Json::Reader reader;
-            Json::Value obj;
-            reader.parse(file_input, obj);
-            const Json::Value& jsonofarticles =  obj["response"]["docs"]; // 
-            for (int i = 0; i < jsonofarticles.size(); i++){
-                string abstract = jsonofarticles[i]["abstract"].asString();
-                string lead_paragraph =  jsonofarticles[i]["lead_paragraph"].asString();
-                string abstract_leadparagraph = abstract;   // lead_paragraph;
-
-                std::string word;
-                for (auto letter : abstract_leadparagraph){
-
-                    if (letter == ' ' or letter == '.' or letter ==','){
-                        if ((!word.empty())){
-                            singleString += word +' ';
-                            word.clear();
-                        }
-                }       
-                else if (isalpha(letter) ){
+                else if (isalpha(letter))
+                {
                     char letter_lowercase = tolower(letter);
                     word = word + letter_lowercase;
-                } }}
+                }
+            }
+        }
 
-                singleString[singleString.size() - 1] = '$'; // adding terminator
+        singleString[singleString.size() - 1] = '$'; // adding terminator
 
-                return singleString ;}  
+        return singleString;
+    }
 };
 
+int main()
+{
+    APICall api_instance; // Create an object of APICallAndParser - class
+    std::string apiadress = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=";
+    string authkey = getenv("AUTH_KEY"); // get authentication key for API
+
+    api_instance.apicall(apiadress, authkey);        // Calling API and storing data in json
+    string finalString = api_instance.dataparsing(); // parses data from json file and creates vector of string
+    cout << finalString << endl;
+
+    TrieNode myObj;
+
+    TrieNode *curr = new TrieNode(); // current node
+
+    for (int i = 0; i < finalString.size(); i++)
+    {
+        myObj.insert(curr, finalString.substr(i));
+    }
+
+    // search is string exists in trie
+
+    string searchString;
+    cout << " " << endl;
+    cout << "Type a word or sentence to search in the suffixtrie:";
+    cin >> searchString;
+
+    auto [isSubstring_search, location_search] = myObj.search(curr, searchString);
+    if (isSubstring_search)
+    {
+        cout << "Yes! " << location_search << endl;
+    }
+    else
+    {
+        cout << "No " << location_search << endl;
+    }
 
 
+    string finalString_a = "mejl meomeo moikka meika maatalous ";
+    TrieNode myObj_autocomplete;
+    TrieNode *curr_autocomplete = new TrieNode(); // current node
+    std::string word;
 
+    for (int i = 0; i < finalString_a.size(); i++)
+    {
+        if (finalString_a[i] == ' ')
+        {
+            word += '$';
+            cout << word << endl;
 
-int main() {
-        APICall api_instance;  // Create an object of APICallAndParser - class
-        std::string apiadress = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=";
-        string authkey = getenv("AUTH_KEY");  // get authentication key for API
-
-        api_instance.apicall(apiadress, authkey);  // Calling API and storing data in json
-        string finalString = api_instance.dataparsing(); // parses data from json file and creates vector of string
-        cout << finalString << endl;
-       
-        TrieNode myObj;
-       
-        TrieNode* curr= new TrieNode(); // current node
-        
-            for (int i = 0; i < finalString.size(); i++){
-                myObj.insert(curr, finalString.substr(i));
-                
-            }
-        
-        // search is string exists in trie
-        
-        string searchString;
-        cout << " " << endl;
-        cout << "Type a word or sentence to search in the suffixtrie:";
-        cin >> searchString; 
-      
-        auto [isSubstring_search, location_search] = myObj.search(curr, searchString);
-        if (isSubstring_search){
-            cout << "Yes! " << location_search << endl;
+            myObj_autocomplete.insert(curr_autocomplete, word);
+            word = "";
         }
-        else{
-            cout << "No "<< location_search << endl;
+        else
+        {
+            word = word + finalString_a[i];
         }
+    }
 
-       
-            
-        // autocomplete
-        TrieNode myObj_autocomplete; 
-        TrieNode* curr_autocomplete= new TrieNode(); // current node
-        std:: string word;
-        
-            for (int i = 0; i < finalString.size(); i++){
-                if (finalString[i] == ' '){
-                    word +='$';
-                    cout << word << endl;
-                    
-                    myObj_autocomplete.insert(curr_autocomplete, word); 
-                    word = "";
-
-                }
-                else{
-                     word = word + finalString[i];  
-
-                }      
-            }
-        
-        // search is string exists in trie
-
-      // auto complete
-
-        string autoCompleteString;
-        cout << " " << endl;
-        cout << "Type a word or sentence for the suffixtrie to autocomplete: (in progress, not working yet)";
-        cin >> autoCompleteString; 
-        auto [isSubstring_autocomplete, location_autocomplete] = myObj_autocomplete.search(curr_autocomplete, autoCompleteString);
-        if (isSubstring_autocomplete){
-            vector <string> suggestions = myObj_autocomplete.preorder(location_autocomplete, autoCompleteString);
-            for (int i = 0; i < suggestions.size(); i++){
-                cout << suggestions[i] << endl;
-                
-            }  
+    string autoCompleteString;
+    cout << " " << endl;
+    cout << "Type a word or sentence for the suffixtrie to autocomplete: (in progress, not working yet)";
+    cin >> autoCompleteString;
+    auto [isSubstring_autocomplete, location_autocomplete] = myObj_autocomplete.search(curr_autocomplete, autoCompleteString);
+    if (isSubstring_autocomplete)
+    {
+        vector<string> suggestions = myObj_autocomplete.preorder(location_autocomplete, autoCompleteString);
+        for (int i = 0; i < suggestions.size(); i++)
+        {
+            cout << suggestions[i] << endl;
         }
-        else {
-            cout << "query prefix not present"<< endl;
-        }
+    }
+    else
+    {
+        cout << "query prefix not present" << endl;
+    }
 
-        return 0;}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    return 0;
+}
--- suffixtrie_hashmap.cpp
+++ suffixtrie_hashmap.cpp
@@ -2,92 +2,76 @@
 #include <tuple>
 using namespace std;
 
-
-
-
-class TrieNode{
-    public:
-    unordered_map<char,TrieNode*> hashmap;
+class TrieNode
+{
+public:
+    unordered_map<char, TrieNode *> hashmap;
     std::string s;
     std::string alphabet = "$abcdefghijklmnopqrstuvwxyz";
 
-    void insert(TrieNode* &root,string suffix){
+    void insert(TrieNode *&root, string suffix)
+    {
         TrieNode *curr = root;
-           
         char letter;
-        for (int i = 0; i < suffix.size(); i++){
+        for (int i = 0; i < suffix.size(); i++)
+        {
             letter = suffix[i];
-             if (curr->hashmap.find(letter) == curr->hashmap.end()){
-                    curr->hashmap[letter] = new TrieNode;
-            }  
+            if (curr->hashmap.find(letter) == curr->hashmap.end())
+            {
+                curr->hashmap[letter] = new TrieNode;
+            }
             curr = curr->hashmap[letter];
-           /*  cout << curr << endl; */
-                 
-        }                         
-     }
+        }
+    }
 
+    tuple<bool, TrieNode *> search(TrieNode *root, string s)
+    {
+        TrieNode *curr = root;
 
-
-     tuple<bool, TrieNode*> search(TrieNode* root, string s){  
-        TrieNode* curr = root;
-      
-        for (int i = 0; i< s.size(); i++){
-            /* cout << s[i] << endl; */
-            if(curr -> hashmap.find(s[i]) == curr -> hashmap.end()){
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (curr->hashmap.find(s[i]) == curr->hashmap.end())
+            {
                 return {0, root};
             }
             curr = curr->hashmap[s[i]]; // update pointer
         }
-        return {1, curr};    
+        return {1, curr};
     }
 
-
-
-    void dfs_helper(TrieNode* root, vector <string> words, string stringBuilder, int level, string original){  // recursion private
-    
-    TrieNode* curr = root;
-    for (int i = 0; i< alphabet.size(); i++){
-        /* cout << s[i] << endl; */
-        if(!(curr -> hashmap.find(alphabet[i]) == curr -> hashmap.end())){
-            if(alphabet[i] == '$'){
-                
-                string w = original + stringBuilder;
-                words.push_back(w);
-                cout << "$: " << w << endl;
-                
-            }else{
-            
-            cout << "FOUND" << alphabet[i] << endl;
-            stringBuilder += alphabet[i];
-            curr = curr -> hashmap[alphabet[i]];
-            dfs_helper(curr, words, stringBuilder, level, original);
-         
-           
-            }
-           
-        }
-    
-    }
-   
-    }
-
-
-    vector <string> preorder(TrieNode* root, string originalString){ // root node 
+    void dfs_helper(TrieNode *root, vector<string> words, string stringBuilder, int level, string original)
+    { 
        
-    vector <string> words;  // string builder
-    int level = 0;  // current level index/ depth within trie
-    std::string stringBuilder;
-    dfs_helper(root, words, stringBuilder, level, originalString);
-    
-            // process rot & call reorder on each node in child
+        for (int i = 0; i < alphabet.size(); i++)
+        {
+            if (root->hashmap.find(alphabet[i]) == root->hashmap.end())
+            {
+            }
+            else
+            {
+                if (alphabet[i] == '$')
+                {
+                    string final = original + stringBuilder;
+                    cout << final << endl;
+                    stringBuilder = "";
+                    
+                }
+                stringBuilder += alphabet[i];
 
-         // If the last matching node of the query has no children, return.
+                TrieNode *curr;
+                curr = root->hashmap[alphabet[i]];
+                dfs_helper(curr, words, stringBuilder, level + 1, original);
+            }     
+        }
+    }
 
-         // Else recursively print all nodes under a subtree of last matching node.
+    vector<string> preorder(TrieNode *root, string originalString)
+    {
+        vector<string> words; 
+        int level = 0;       
+        std::string stringBuilder;
+        dfs_helper(root, words, stringBuilder, level, originalString);
 
-    return words;
-
+        return words;
     }
 };
-
-
